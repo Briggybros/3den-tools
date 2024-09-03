0488a8253ddec6df42418c32afa0236512ebaee2
Gregory Sims <briggybros.2@googlemail.com>
2024-09-03 21:51:25 +0100

    Init mod

---
+++ addons/main/config.cpp
@@ -0,0 +1,15 @@
+
+#include "script_component.hpp"
+
+class CfgPatches {
+	class ADDON {
+		name = CSTRING(component);
+		author = "117Briggy";
+		url = "https://github.com/Briggybros/3den-tools";
+		requiredVersion = 1.82; 
+		requiredAddons[] = {};
+		units[] = {};
+		weapons[] = {};
+		VERSION_CONFIG;
+	};
+};
---
+++ addons/main/script_mod.hpp
@@ -0,0 +1,10 @@
+#define MAINPREFIX Briggybros
+#define PREFIX 3dentools
+
+#include "script_version.hpp"
+
+#define VERSION     MAJOR.MINOR
+#define VERSION_STR MAJOR.MINOR.PATCHLVL.BUILD
+#define VERSION_AR  MAJOR,MINOR,PATCHLVL,BUILD
+
+#define REQUIRED_VERSION 2
